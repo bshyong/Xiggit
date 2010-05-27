7b6dae2f79de15f5d2c3bfb32126c8ea846ed43c
Benjamin Shyong <likeachild@gmail.com>
2010-05-27 13:43:32 -0400

    tweaked list view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,10 @@ class SearchController < ApplicationController
 layout 'application'
 
 def keyword_search
-		@results = BookPost.find_with_index('watterson')
-
+	# @results = BookPost.find_with_index(params[:terms])
+	
+	 @book_posts = BookPost.paginate_search params[:terms], :page => 1, :per_page => 5
+	 @book_bag = find_bag
 end
 
 
