ca113b0046791d2adef81049673931e489bdf375
Benjamin Shyong <likeachild@gmail.com>
2010-05-28 01:10:42 -0400

    added paginated searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,7 @@ layout 'application'
 
 def index
 	# @results = BookPost.find_with_index(params[:terms])
-	 @results = BookPost.paginate_search params[:terms], :page => 1, :per_page => 5
+	 @results = BookPost.paginate_search params[:terms], :per_page => 1, :page => params[:page]
 	 @book_bag = find_bag
 end
 
