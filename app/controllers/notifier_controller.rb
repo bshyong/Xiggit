b1f27cc37baf4f9d41c502800303b6518469d3a0
Benjamin Shyong <likeachild@gmail.com>
2010-06-08 00:25:35 -0400

    added advanced search view

--- app/controllers/notifier_controller.rb
+++ app/controllers/notifier_controller.rb
@@ -4,8 +4,8 @@ class NotifierController < ApplicationController
 
     def contact_seller
         # heroku doesn't give us free workers...
-        Notifier.delay.deliver_contact_seller(params[:email], params[:sender_email], params[:sender_name], params[:book_title])
-     #  Notifier.deliver_contact_seller(params[:email], params[:sender_email], params[:sender_name], params[:book_title])
+     #   Notifier.delay.deliver_contact_seller(params[:email], params[:sender_email], params[:sender_name], params[:book_title])
+       Notifier.deliver_contact_seller(params[:email], params[:sender_email], params[:sender_name], params[:book_title])
          render :update do |page|
             page.replace_html 'contact_seller_link_' + params[:post_id], :partial => 'user/seller_contacted'
             page.visual_effect :highlight, 'contact_seller_link_' + params[:post_id]
